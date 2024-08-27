8f16bfc898a260ebf3eb0e9abfa546983a1f657f
JvIadEye <135623930+JvIadEye@users.noreply.github.com>
2024-08-27 20:42:13 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,6 +1,6 @@
-def yumInstallApp(){
-    echo "Yum installing ..."
-    sh "yum install httpd -y"
+def aptInstallApp(){
+    echo "Apt installing ..."
+    sh "apt install httpd -y"
 }
 
 def buildApp(){
